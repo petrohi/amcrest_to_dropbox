99b00e04f5c500462b80ba6cb8592c41fb92e514
Peter Hizalev <peter.hizalev@gmail.com>
2024-02-11 20:31:30 -0800

    Use rclone to store media

--- sync_dropbox.py
+++ sync_dropbox.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import dropbox
 import os
 import sys
 import shutil
@@ -14,39 +13,12 @@ def read_config(config_file_name):
 
 config = read_config(sys.argv[1])
 
-DROPBOX_APP_KEY = os.environ['DROPBOX_APP_KEY']
-DROPBOX_CHUNK_SIZE = 16 * 1024 * 1024
-
-def upload_to_dropbox(file_path, dest_path):
-    with dropbox.Dropbox(oauth2_refresh_token=config['dropbox']['refresh_token'], app_key=DROPBOX_APP_KEY) as dbx:
-        file_size = os.path.getsize(file_path)
-
-        with open(file_path, 'rb') as f:
-            if file_size <= DROPBOX_CHUNK_SIZE:
-                dbx.files_upload(f.read(), path=dest_path, mode=dropbox.files.WriteMode.overwrite)
-            else:
-                upload_session_start_result = dbx.files_upload_session_start(f.read(DROPBOX_CHUNK_SIZE))
-                cursor = dropbox.files.UploadSessionCursor(session_id=upload_session_start_result.session_id,
-                                                        offset=f.tell())
-                commit = dropbox.files.CommitInfo(path=dest_path, mode=dropbox.files.WriteMode.overwrite)
-
-                while f.tell() < file_size:
-                    if ((file_size - f.tell()) <= DROPBOX_CHUNK_SIZE):
-                        dbx.files_upload_session_finish(f.read(DROPBOX_CHUNK_SIZE),
-                                                        cursor,
-                                                        commit)
-                    else:
-                        dbx.files_upload_session_append(f.read(DROPBOX_CHUNK_SIZE),
-                                                        cursor.session_id,
-                                                        cursor.offset)
-                        cursor.offset = f.tell()
-
 DATE_REGEX = re.compile('([0-9]{4})-([0-9]{2})-([0-9]{2})')
 HOUR_REGEX = re.compile('[0-9]{2}')
 MP4_REGEX = re.compile('.+\.mp4$')
 UNFINISHED_MP4_REGEX = re.compile('.+\.mp4_$')
 
-def scan(camera_name, camera_dir, dropbox_folder):
+def scan(camera_name, camera_dir, rclone_remote):
     date_dirs = os.listdir(camera_dir)
     for date_dir in date_dirs:
         date_match = DATE_REGEX.match(date_dir)
@@ -77,9 +49,9 @@ def scan(camera_name, camera_dir, dropbox_folder):
                             os.system('ffmpeg -y -safe 0 -f concat -i /tmp/ffmpeg_concat_list.txt -vcodec copy -acodec copy /tmp/ffmpeg_concat.mp4')
                             os.remove('/tmp/ffmpeg_concat_list.txt')
 
-                            dropbox_path = dropbox_folder + '/' + camera_name + '/' + year + '/' + month + '/' + day + '/' + hour_dir + '.mp4'
-                            print(camera_name + ' upload: ' + dropbox_path)
-                            upload_to_dropbox('/tmp/ffmpeg_concat.mp4', dropbox_path)
+                            rclone_path = rclone_remote  + ':' + camera_name + '/' + year + '/' + month + '/' + day + '/' + hour_dir + '.mp4'
+                            print(camera_name + ' upload: ' + rclone_path)
+                            os.system('rclone copyto /tmp/ffmpeg_concat.mp4 "' + rclone_path + '"')
                             os.remove('/tmp/ffmpeg_concat.mp4')
 
                             shutil.rmtree(mp4s_dir)
@@ -87,8 +59,8 @@ def scan(camera_name, camera_dir, dropbox_folder):
                             print(camera_name + ' unfinished: ' + mp4s_dir)
 
 cameras_base_dir = config['cameras']['dir']
-dropbox_folder = config['dropbox']['folder']
+rclone_remote = config['rclone']['remote']
 for camera, props in config['cameras'].items():
     if type(props) is dict:
         serial = props['serial']
-        scan(camera, cameras_base_dir + '/' + serial, dropbox_folder)
+        scan(camera, cameras_base_dir + '/' + serial, rclone_remote)
